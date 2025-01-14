3707fb2ad65ce445cca967f2ea9f6b19bf230b21
Ashok Mehta <78004850+ashok0425@users.noreply.github.com>
2025-01-14 14:53:51 +0545

    update rate option

--- public/backend/js/script.js
+++ public/backend/js/script.js
@@ -94,47 +94,49 @@
 /***/ (function(module, exports) {
 
 (function ($) {
-  "use strict"; // Start of use strict
-  // Toggle the side navigation
+  "use strict";
 
+  // Start of use strict
+
+  // Toggle the side navigation
   $("#sidebarToggle, #sidebarToggleTop").on('click', function (e) {
     $("body").toggleClass("sidebar-toggled");
     $(".sidebar").toggleClass("toggled");
-
     if ($(".sidebar").hasClass("toggled")) {
       $('.sidebar .collapse').collapse('hide');
     }
-
     ;
-  }); // Close any open menu accordions when window is resized below 768px
+  });
 
+  // Close any open menu accordions when window is resized below 768px
   $(window).resize(function () {
     if ($(window).width() < 768) {
       $('.sidebar .collapse').collapse('hide');
     }
-
     ;
-  }); // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
+  });
 
+  // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
   $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function (e) {
     if ($(window).width() > 768) {
       var e0 = e.originalEvent,
-          delta = e0.wheelDelta || -e0.detail;
+        delta = e0.wheelDelta || -e0.detail;
       this.scrollTop += (delta < 0 ? 1 : -1) * 30;
       e.preventDefault();
     }
-  }); // Scroll to top button appear
+  });
 
+  // Scroll to top button appear
   $(document).on('scroll', function () {
     var scrollDistance = $(this).scrollTop();
-
     if (scrollDistance > 100) {
       $('.scroll-to-top').fadeIn();
     } else {
       $('.scroll-to-top').fadeOut();
     }
-  }); // Smooth scrolling using jQuery easing
+  });
 
+  // Smooth scrolling using jQuery easing
   $(document).on('click', 'a.scroll-to-top', function (e) {
     var $anchor = $(this);
     $('html, body').stop().animate({
@@ -142,26 +144,21 @@
     }, 1000, 'easeInOutExpo');
     e.preventDefault();
   });
-
   if ($('.select2').length > 0) {
     $('.select2').select2();
   }
-
   if ($('.datepicker').length > 0) {
     $('.datepicker').datepicker({
       autoclose: true
     });
   }
-
   $('.sluggable').on('input', function (e) {
     e.preventDefault();
     var target = $(this).data('slug');
-
     if (target) {
       $(target).val($(this).val().toLowerCase().replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '-'));
     }
   });
-
   $.fn.confirmDelete = function (formId) {
     swal({
       title: "Are you sure?",
@@ -175,7 +172,6 @@
       }
     });
   };
-
   $.fn.confirm = function (url) {
     swal({
       title: "Are you sure?",
@@ -189,7 +185,6 @@
       }
     });
   };
-
   $.fn.confirmRestore = function (formId) {
     swal({
       title: "Are you sure?",
@@ -203,7 +198,6 @@
       }
     });
   };
-
   if (window.matchMedia("(max-width: 767px)").matches) {
     $("body").toggleClass("sidebar-toggled");
     $("#accordionSidebar").toggleClass("toggled");
